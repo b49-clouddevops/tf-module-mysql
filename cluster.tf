5892e34015bb26e9800a7742e6f2be2a9deceea6
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 07:44:54 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,10 +1,10 @@
 # Creating RDS Instance, a managed service for sequel db
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
+  name                 = "dummy"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
