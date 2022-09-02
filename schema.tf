41234bedc7e1974b16d0ec3c86d62ce1fee30061
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 08:41:16 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -6,7 +6,7 @@ resource "null_resource" "schema" {
     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
     unzip -o mysql.zip 
     cd mysql-main 
-    mysql -h ${aws_db_instance.mysqldb.address}  -uadmin1 -pRoboShop1 <shipping.sql
+    mysql -h ${aws_db_instance.mysqld.address}  -uadmin1 -pRoboShop1 <shipping.sql
 
 EOF
 
