19f32e1a423ce7bbcd5d3334df97806bb7523617
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 23:12:48 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -11,6 +11,9 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.mysql-sb.name 
   vpc_security_group_ids = [aws_security_group.allow_mysql.id]
+  depends_on            = [aws_security_group.allow_mysql]
+    
+  ]
 
 }
 
