a751a53b202c5acffbc2b2d4cbc9add96265627e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-04 07:37:28 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "mysql" {
   identifier           = "roboshop-mysql-${var.ENV}"  
   allocated_storage    = var.RDS_MYSQL_CAPACITY
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       =  RDS_ENGINE_VERSION
   instance_class       = "db.t3.micro"
   username             = "admin1"
   password             = "RoboShop1"
